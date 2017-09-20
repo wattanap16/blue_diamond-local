3ad69260aa3a66da6f4dc1901d2b7943c8164539
bank wattanap <test@test.com>
2017-09-19 23:43:33 -0700

    after #4 Seeding Data (WORK with MongoDB)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'LOVE FAIRY' });
+
+  Product.find(function(err, docs){
+
+       var productRows = [];
+       var rowSize = 3;
+       for(var i = 0; i < docs.length; i += rowSize) {
+         productRows.push(docs.slice(i, i + rowSize));
+       }
+       res.render('shop/index', { title: 'LOVE FAIRY', products: productRows });
+  });
+  
 });
 
 module.exports = router;
