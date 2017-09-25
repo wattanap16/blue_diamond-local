b1d301a48702949579c5978a98c7565921fcdb93
bank wattanap <test@test.com>
2017-09-24 22:14:08 -0700

    after #12 Cart Model

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Cart = require('../models/cart');
 
 
 
@@ -21,6 +22,20 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/add-to-cart/:id', function(req, res, next) {
+   
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {} );
 
+  Product.findById(productId, function(err, product) {
+       if (err) {
+              return  res.redirect('/');
+       }
+       cart.add(product, product.id);
+       req.session.cart = cart;
+       console.log(req.session.cart);
+       res.redirect('/');
+  });
+});
 
 module.exports = router;
