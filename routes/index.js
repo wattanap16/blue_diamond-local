7b25f1e94b53dd4aba4e202fe36db4064c86013a
bank wattanap <test@test.com>
2017-09-26 22:22:15 -0700

    after #18 Forcing User Login

--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,7 @@ router.get('/shopping-cart', function(req, res, next) {
                    totalPrice: cart.totalPrice});
 });
 
-router.get('/checkout', function(req, res, next) {
+router.get('/checkout', isLoggedIn, function(req, res, next) {
       if (!req.session.cart) {
             return res.redirect('/shopping-cart');
       }
@@ -61,7 +61,7 @@ router.get('/checkout', function(req, res, next) {
       res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
 });
 
-router.post('/checkout', function(req, res, next) {
+router.post('/checkout', isLoggedIn, function(req, res, next) {
   if (!req.session.cart) {
       return res.redirect('/shopping-cart');
   }
@@ -95,3 +95,11 @@ router.post('/checkout', function(req, res, next) {
 });
 
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.session.oldUrl = req.url;
+    res.redirect('/user/signin');
+}
--- routes/user.js
+++ routes/user.js
@@ -31,10 +31,18 @@ router.get('/signup', function(req, res, next){
 });
 
 router.post('/signup', passport.authenticate('local.signup', {
-   successRedirect: '/user/profile',
+   //successRedirect: '/user/profile',
    failureRedirect: '/user/signup',
    failureFlash: true
-}));
+}), function(req, res, next) {
+    if (req.session.oldUrl) {
+        var oldUrl = req.session.oldUrl;
+        req.session.oldUrl = null;
+        res.redirect(oldUrl);
+    } else {
+        res.redirect('/user/profile');
+    }
+});
 
 
 router.get('/signin', function(req, res, next) {
@@ -43,13 +51,21 @@ router.get('/signin', function(req, res, next) {
     {csrfToken: req.csrfToken(), 
         messages: messages, 
         hasErrors: messages.length > 0 });
-    });
+});
     
 router.post('/signin', passport.authenticate('local.signin', {
-    successRedirect: '/user/profile',
+    //successRedirect: '/user/profile',
     failureRedirect: '/user/signin',
     failureFlash: true
-}));
+}), function(req, res, next) {
+      if (req.session.oldUrl) {
+        var oldUrl = req.session.oldUrl;
+        req.session.oldUrl = null;
+        res.redirect(oldUrl);
+      } else {
+          res.redirect('/user/profile');
+      }
+});
 
 
 
